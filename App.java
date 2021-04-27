00f0546c480c7fe0dcdc30559351f848fb5077fa
RajuTanneru <47772928+RajuTanneru@users.noreply.github.com>
2021-04-27 11:50:47 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,30 @@
+package maven_project.Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void  pyramidPattern(int n) 
+    {
+    	
+    	 for (int i=0; i<n; i++) //outer loop for number of rows(n) { for (int j=n-i; j>1; j--) //inner loop for spaces
+         { 
+             System.out.print(" "); //print space
+         }  
+		for (int j=0; j<=n; j++) //inner loop for number of columns
+         { 
+             System.out.print("* "); //print star
+         } 
+
+         System.out.println(); //ending line after each row
+     } 
+ 
+
+ public static void main(String args[]) //driver function
+ { 
+     int n = 5; 
+     pyramidPattern(n);
+    }
+}
